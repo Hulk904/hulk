e373f855da867ae812d59153a8d406f025a33a13
yangyuan <yangyuan3@jd.com>
2017-08-01 15:04:17 +0800

    update

---
+++ src/main/com/jd/controller/FirstController.java
@@ -0,0 +1,21 @@
+package main.com.jd.controller;
+
+import org.smart4j.framework.mvc.annotation.Action;
+import org.smart4j.framework.mvc.annotation.Request;
+import org.smart4j.framework.mvc.bean.Result;
+
+import java.util.Date;
+
+/**
+ * Created by yangyuan on 7/27/17.
+ */
+@Action
+public class FirstController {
+
+    @Request.Get(value = "/first/hello.do")//地址要跟浏览器后面的uri一样，包括 /  斜杠也不能丢
+    public Result  hello(){
+        Result result = new Result(true);
+        result.setData(new Date());
+        return result;
+    }
+}
